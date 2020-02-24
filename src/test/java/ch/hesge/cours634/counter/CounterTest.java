e6fcb929b771db8c138dec2417d90a30cd432579
Maryo <mario.crocop@hotmail.com>
2020-02-24 09:29:03 +0100

    Ajout des Test

---
+++ src/test/java/ch/hesge/cours634/counter/CounterTest.java
@@ -0,0 +1,31 @@
+package ch.hesge.cours634.counter;
+
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @org.junit.Test
+    public void inc() throws CounterException {
+        Counter counter = new Counter();
+        counter.inc();
+        Assert.assertEquals(1,counter.getValue()); //On vérifie si la valeur est correct
+    }
+
+    @org.junit.Test
+    public void add() throws CounterException {
+        Counter counter = new Counter();
+        counter.add(2);
+        Assert.assertEquals(2,counter.getValue()); //On vérifie si la valeur est correct
+    }
+
+    @org.junit.Test
+    public void checkMin() {
+
+    }
+
+    @org.junit.Test
+    public void getValue() {
+    }
+}
